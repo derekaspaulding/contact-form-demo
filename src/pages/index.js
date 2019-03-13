a506c9b27f826b45b1b4073c1875167b1ebd693e
Derek Spaulding <derek@derekaspaulding.com>
2019-03-12 22:08:43 -0500

    add contact form with Formik

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,33 @@
-import React from "react"
+import React from "react";
+import { Formik, Form, Field } from 'formik';
 
+export default () => (
+  <Formik
+    initialValues={{
+      name: '',
+      email: '',
+      message: '',
+    }}
+    onSubmit={(values, actions) => {
+      console.log('submit')
+      alert(JSON.stringify(values, null, 2));
+      actions.setSubmitting(false);
+    }}
+import React from "react"
+  >
+  {() => (
+    <Form>
+      <label htmlFor="name">Name: </label>
+      <Field name="name" />
+      <label htmlFor="email">Email: </label>
+      <Field name="email" />
+
+      <label htmlFor="message">Message: </label>
+      <Field name="message" component="textarea"/>
 export default () => <div>Hello world!</div>
+
+      <button type="submit">Send</button>
+    </Form>
+  )}
+  </Formik>
+)
